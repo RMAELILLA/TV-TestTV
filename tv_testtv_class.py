ed4233bcc51098aca1ccd437afa479fd79d5c26c
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-17 22:23:01 +0800

    use decision statement

--- tv_testtv_class.py
+++ tv_testtv_class.py
@@ -1,24 +1,25 @@
+print("Television")
+int(input("Choose 0-1: "))
 # create class named TV
 class TV:
 
 # current channel (1 to 120)
 # current volume level (1 to 7)
-    def __init__(self, channel = [1, 120], volume = [1, ]):
+    def __init__(self, channel = [1, 120], volume = [1, 7], tvOn = 1):
         self.channel = channel
         self.volume = volume
+        self.tvOn = tvOn
 
-# indicate if on/off
-# create TestDriver program TestTV
-# create two Class TV objects
-# construct a default TV object
-# turns on TV
-# turns off TV
-# returns channel
-# set new channel
-# gets volume level
-# set new volume level
-# increases channel number by 1
-# decreases channel number by 1
-# increases channel number by 1
-# decreases channel number by 1
-# print output
+    # create two Class TV objects
+    # construct a default TV object
+    # create TestDriver program TestTV
+    # indicate if on/off
+    # returns channel
+    # set new channel
+    # gets volume level
+    # set new volume level
+    # increases channel number by 1
+    # decreases channel number by 1
+    # increases channel number by 1
+    # decreases channel number by 1
+    # print output

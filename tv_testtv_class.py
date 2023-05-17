381244af0246c62ab4123ac8a8c5faa1676d0c06
RMAELILLA <ronioryzamae12@gmail.com>
2023-05-17 20:48:54 +0800

    planning

---
+++ tv_testtv_class.py
@@ -0,0 +1,18 @@
+# create class named TV
+# current channel (1 to 120)
+# current volume level (1 to 7)
+# indicate if on/off
+# create TestDriver program TestTV
+# create two Class TV objects
+# construct a default TV object
+# turns on TV
+# turns off TV
+# returns channel
+# set new channel
+# gets volume level
+# set new volume level
+# increases channel number by 1
+# decreases channel number by 1
+# increases channel number by 1
+# decreases channel number by 1
+# print output
